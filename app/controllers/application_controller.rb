c611b5b30b160fbf934de75a10de49bcf2653317
sekai5108 <sekai.5108@gmail.com>
2021-10-22 03:25:27 +0000

    Add 4-10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_paramitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, key: [:name])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  resources :books
+
   devise_for :users
-  root to: 'home#top'
-  
-  
-  
+
+
+get '/', to: 'homes#top'
+get '/home/about', to: 'homes#about'
 end
